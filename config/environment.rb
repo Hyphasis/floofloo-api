6436f959da0202ac9462900468dccc582d0b26f6
tienyulin <t.tienyulin@gmail.com>
2021-11-01 20:30:39 +0800

    feat: add figaro

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,30 @@
 
 require 'roda'
 require 'yaml'
+require 'sequel'
+require 'figaro'
 
 module Floofloo
   # Configuration for the App
   class App < Roda
-    CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-    NEWS_KEY = CONFIG['NEWS_KEY']
+    plugin :environments
+
+    configure do
+      Figaro.application = Figaro::Application.new(
+        environment: environment,
+        path: File.expand_path('config/secrets.yml')
+      )
+      Figaro.load
+
+      def self.config() = Figaro.env
+      
+      configure :development, :test do
+        ENV['DATABASE_URL'] = "sqlite://#{config.DB_FILENAME}"
+      end
+
+      # Database Setup
+      DB = Sequel.connect(ENV['DATABASE_URL'])
+      def self.DB() = DB # rubocop:disable Naming/MethodName
+    end
   end
 end
