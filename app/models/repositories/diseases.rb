4f396d37bc8555d0d34944814df0999fd214a290
tienyulin <t.tienyulin@gmail.com>
2021-11-05 20:13:17 +0800

    feat: add create method for disease repository

--- app/models/repositories/diseases.rb
+++ app/models/repositories/diseases.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module FlooFloo
+module Floofloo
   module Repository
     # Repository for Members
     class Diseases
@@ -20,41 +20,16 @@ module FlooFloo
         rebuild_entity Database::DiseaseOrm.first(name: name)
       end
 
+      def self.create(entity)
+        Database::DiseaseOrm.find_or_create(entity.to_attr_hash)
+      end
+
       def self.rebuild_entity(db_record)
         return nil unless db_record
 
         Entity::Disease.new(
           name: db_record.name
         )
-      end
-
-      def self.rebuild_many(db_records)
-        db_records.map do |db_disease|
-          Diseases.rebuild_entity(db_disease)
-        end
-      end
-
-      def self.db_find_or_create(entity)
-        Database::DiseaseOrm.find_or_create(entity.to_attr_hash)
-      end
-
-      # Helper class to persist project and its members to database
-      class PersistDisease
-        def initialize(entity)
-          @entity = entity
-        end
-
-        def create_disease
-          Database::DiseaseOrm.create(@entity.to_attr_hash)
-        end
-
-        def call
-          news = News.db_find_or_create(@entity.news)
-
-          create_disease.tap do |db_news|
-            db_news.update(news: news)
-          end
-        end
       end
     end
   end
