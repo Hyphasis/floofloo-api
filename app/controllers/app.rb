5ff01cb0c95e312ab95932da71498750f22d94c0
tienyulin <t.tienyulin@gmail.com>
2021-11-16 18:52:30 +0800

    feat: add error message flash bar

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,15 +2,18 @@
 
 require 'roda'
 require 'slim'
+require 'slim/include'
 
 module Floofloo
   # Web App
   class App < Roda
-    plugin :render, engine: 'slim', views: 'app/views'
-    plugin :assets, css: 'style.css', path: 'app/views/assets'
     plugin :halt
-    plugin :all_verbs
     plugin :flash
+    plugin :all_verbs # recognizes HTTP verbs beyond GET/POST (e.g., DELETE)
+    plugin :render, engine: 'slim', views: 'app/presentation/views_html'
+    plugin :public, root: 'app/presentation/public'
+    plugin :assets, path: 'app/presentation/assets',
+                    css: 'style.css', js: 'table_row.js'
 
     route do |routing| # rubocop:disable Metrics/BlockLength
       routing.assets # load CSS
@@ -51,15 +54,25 @@ module Floofloo
 
                 # Put the news on screen (Home page)
                 routing.redirect "/event/#{event_name}/news/#{news_result.id}"
+              rescue StandardError => e
+                flash[:error] = 'Failed to get news!'
+                puts e.message
+
+                routing.redirect '/'
               end
 
-              routing.get do
+              routing.get do # rubocop:disable Metrics/BlockLength
                 # GET /event/{event_name}/news/{news_id}
                 routing.on String do |news_id|
                   news = Repository::ArticlesFor.klass(Entity::News)
                     .find_id(news_id)
 
                   view 'news', locals: { news: news }
+                rescue StandardError => e
+                  flash[:error] = 'Failed to get news!'
+                  puts e.message
+
+                  routing.redirect '/'
                 end
 
                 # GET /event/{event_name}/news?language={language}&keywords={keywords}&from={from}&to={to}
@@ -71,6 +84,11 @@ module Floofloo
                   to = routing.params['to']
                   sort_by = routing.params['sort_by']
 
+                  if keywords.nil? || keywords.empty?
+                    flash[:error] = 'Please enter keywords'
+                    routing.redirect '/'
+                  end
+
                   session[:keywords].insert(0, keywords).uniq!
 
                   news = News::NewsMapper
@@ -78,6 +96,11 @@ module Floofloo
                     .find(language, keywords, from, to, sort_by)
 
                   view 'news', locals: { news: news }
+                rescue StandardError => e
+                  flash[:error] = 'Failed to get news!'
+                  puts e.message
+
+                  routing.redirect '/'
                 end
               end
             end
@@ -93,16 +116,26 @@ module Floofloo
                 .find(keywords)
 
               view 'donations', locals: { donations: donations }
+            rescue StandardError => e
+              flash[:error] = 'Failed to get donations!'
+              puts e.message
+
+              routing.redirect '/'
             end
           end
 
           routing.on 'delete' do
-            # GET /event/{event_name}
+            # GET /event/{event_name}/delete
             routing.get do
               session[:keywords].delete(event_name)
 
               events_session = session[:keywords]
               view 'home', locals: { events_session: events_session }
+            rescue StandardError => e
+              flash[:error] = 'Failed to delete event!'
+              puts e.message
+
+              routing.redirect '/'
             end
           end
         end
