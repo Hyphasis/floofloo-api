a1a9e45bb377ab9ad8ef3f9306e899887aae480f
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-11-20 16:19:52 +0800

    add: acceptance test

--- spec/tests/acceptance/acceptance_spec.rb
+++ spec/tests/acceptance/acceptance_spec.rb
@@ -24,6 +24,19 @@
      describe 'Visit Home page' do
        it '(HAPPY) should go to homepage' do
          @browser.goto homepage
+#         _(@browser.h1(id: 'main_header').text).must_equal 'Floofloo'
+         _(@browser.text_field(id: 'keywords').present?).must_equal true
+       end
+     end
+
+     describe 'Add News' do
+       it '(HAPPY) should be able to request a news' do
+         @browser.goto homepage
+
+         good_news_keywords = KEYWORDS
+         @browser.text_field(id: 'keywords').set(good_news_keywords)
+         @browser.button(id: 'repo-form-submit').click
+         @browser.url.include? KEYWORDS
        end
      end
    end
