dca05ff5e08ac67b9fdfb4ad6f008c2f156109ba
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-12-04 13:19:14 +0800

    add: integration test for AddNews and AddDonation

---
+++ spec/tests/integration/services/add_donation_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../../../helpers/spec_helper'
+require_relative '../../../helpers/vcr_helper'
+require_relative '../../../helpers/database_helper'
+
+describe 'AddDonation Service Integration Test' do
+  VcrHelper.setup_vcr
+
+  before do
+    VcrHelper.configure_vcr_for_news(recording: :new_episodes)
+  end
+
+  after do
+    VcrHelper.eject_vcr
+  end
+  describe 'Store and find Donation' do
+    before do
+      DatabaseHelper.wipe_database
+      @issue = Floofloo::Services::AddIssue.new.call(issue_name: ISSUE_NAME)
+      @event = Floofloo::Services::AddEvent.new.call(issue_name: ISSUE_NAME, event_name: EVENT_NAME)
+    end
+    it 'HAPPY: should be able to find and save Donation from API to database' do
+      donation_made = Floofloo::Services::AddDonation.new.call(event_name: EVENT_NAME)
+      _(donation_made.success?).must_equal true
+    end
+  end
+end
