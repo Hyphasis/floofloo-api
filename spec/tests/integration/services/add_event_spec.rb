8090cc01aec69f9573b3b28aa3d5d73f99540c80
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-12-27 09:10:52 +0800

    add: get event spec and fix: some spec names

--- spec/tests/integration/services/add_event_spec.rb
+++ spec/tests/integration/services/add_event_spec.rb
@@ -5,27 +5,25 @@ require_relative '../../../helpers/vcr_helper'
 require_relative '../../../helpers/database_helper'
 
 describe 'AddEvent Service Integration Test' do
-  VcrHelper.setup_vcr
-
-  before do
-    VcrHelper.configure_vcr_for_news(recording: :none)
-  end
-
-  after do
-    VcrHelper.eject_vcr
-  end
-  describe 'Store and find event' do
+  describe 'Add Event in database' do
     before do
       DatabaseHelper.wipe_database
       @issue = Floofloo::Services::AddIssue.new.call(issue_name: ISSUE_NAME)
     end
-    it 'HAPPY: should be able to save event in database' do
+    it 'HAPPY: should be able to add event in database' do
       event_made = Floofloo::Services::AddEvent.new.call(issue_name: ISSUE_NAME, event_name: EVENT_NAME)
       _(event_made.success?).must_equal true
+      rebuilt = event_made.value!.message
+      _(rebuilt.name).must_equal(EVENT_NAME)
     end
 
-    it 'SAD: should not be able to save empty event in database' do
+    it 'SAD: should not be able to add an empty event in database' do
       event_made = Floofloo::Services::AddEvent.new.call()
+      _(event_made.success?).must_equal false
+    end
+
+    it 'SAD: should not be able to add an event under inexistent issue' do
+      event_made = Floofloo::Services::AddEvent.new.call(issue_name: 'Inexistent Issue', event_name: EVENT_NAME)
       _(event_made.success?).must_equal false
     end
   end
--- spec/tests/unit/add_issue_spec.rb
+++ spec/tests/unit/add_issue_spec.rb
@@ -5,20 +5,11 @@ require_relative '../../helpers/vcr_helper'
 require_relative '../../helpers/database_helper'
 
 describe 'AddIssue Service Unit Test' do
-  VcrHelper.setup_vcr
-
-  before do
-    VcrHelper.configure_vcr_for_news(recording: :none)
-  end
-
-  after do
-    VcrHelper.eject_vcr
-  end
-  describe 'Save Issue in database' do
+  describe 'Add Issue in database' do
     before do
       DatabaseHelper.wipe_database
     end
-    it 'HAPPY: should be able to save issue in database' do
+    it 'HAPPY: should be able to add issue in database' do
       issue_made = Floofloo::Services::AddIssue.new.call(issue_name: ISSUE_NAME)
       _(issue_made.success?).must_equal true
 
@@ -26,7 +17,7 @@ describe 'AddIssue Service Unit Test' do
       _(rebuilt.name).must_equal(ISSUE_NAME)
     end
 
-    it 'SAD: should not be able to save empty issue in database' do
+    it 'SAD: should not be able to add an empty issue in database' do
       issue_made = Floofloo::Services::AddIssue.new.call()
       _(issue_made.success?).must_equal false
     end
