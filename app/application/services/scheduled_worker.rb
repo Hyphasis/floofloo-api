17d5f146089f75b7a034e7bbbdde6aef1b330178
Steventime <32660539+Steventime0430@users.noreply.github.com>
2022-01-01 14:43:52 +0800

    fix: scheduled worker service

--- app/application/services/scheduled_worker.rb
+++ app/application/services/scheduled_worker.rb
@@ -6,65 +6,78 @@ require 'json'
 
 module Floofloo
   module Services
-    # Transaction to get news from News API
+    # Transaction to add recommendations of news and donation projects
     class ScheduledWorker
       include Dry::Transaction
 
-      step :delete_data_in_db
-      step :add_news_in_db
-      step :add_donation_in_db
-      step :get_news_from_db
-      step :get_donation_from_db
-      step :add_recommendation_in_db
+      step :delete_data_in_database
+      step :get_event_names
+      step :add_news
+      step :add_donation
+      step :get_news
+      step :add_recommendation
 
       private
 
-      def delete_data_in_db(input)
+      def delete_data_in_database(input)
         Floofloo::Services::DeleteAllRecommendations.new.call
         Floofloo::Services::DeleteAllNews.new.call
         Floofloo::Services::DeleteAllDonation.new.call
         Success(input)
       rescue StandardError
-        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to delete data in DB'))
+        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to delete data in the database.'))
       end
 
-      def add_news_in_db(input)
-        input[:news_list] = Floofloo::Services::AddNewsWorker.new.call(event_name: input[:event_name])
-        Success(input)
-      rescue StandardError
-        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to add news in DB'))
-      end
-
-      def add_donation_in_db(input)
-        input[:donation_list] = Floofloo::Services::AddDonationWorker.new.call(event_name: input[:event_name])
-        Success(input)
-      rescue StandardError
-        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to add donation in DB'))
-      end
-
-      def get_news_from_db(input)
-        input[:news_list] = Services::GetNews.new.call(event_name: input[:event_name]).value!.message.articles
-        Success(input)
-      rescue StandardError => e
-        Failure(Response::ApiResult.new(status: :internal_error, message: e.message))
-      end
-
-      def get_donation_from_db(input)
-        input[:donation_list] = Services::GetDonation.new.call(event_name: input[:event_name]).value!.message.donations
-        Success(input)
-      rescue StandardError => e
-        Failure(Response::ApiResult.new(status: :internal_error, message: e.message))
-      end
-
-      def add_recommendation_in_db(input)
-        input[:news_list].each do |news|
-          input[:donation_list].each do |donation|
-            Floofloo::Services::AddRecommendation.new.call(news_id: news.id, donation_id: donation.id)
+      def get_event_names(input)
+        find_events = Services::GetEvent.new.call.value!.message.events
+        input[:event_names] = []
+        find_events.each do |event|
+          event.themes.each do |event_name|
+            input[:event_names].append(event_name[:name])
           end
         end
-        Success(Response::ApiResult.new(status: :ok, message: 'success'))
+        Success(input)
+      rescue StandardError
+        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to get event names.'))
+      end
+
+      def add_news(input)
+        event_names = input[:event_names]
+        event_names.each do |event_name|
+          Floofloo::Services::AddNewsWorker.new.call(event_name: event_name)
+        end
+        Success(input)
+      rescue StandardError
+        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to add news in database.'))
+      end
+
+      def add_donation(input)
+        event_names = input[:event_names]
+        event_names.each do |event_name|
+          Floofloo::Services::AddDonationWorker.new.call(event_name: event_name)
+        end
+        Success(input)
+      rescue StandardError
+        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to add donation in database.'))
+      end
+
+      def get_news(input)
+        input[:news_list] = Floofloo::Database::NewsOrm.all
+        Success(input)
       rescue StandardError => e
         Failure(Response::ApiResult.new(status: :internal_error, message: e.message))
+      end
+
+      def add_recommendation(input)
+        input[:news_list].each do |news|
+          donation_list = Repository::DonationFor.klass(Entity::Donation).find_event_id(news.event_id)
+          donation_list.each do |donation|
+            Services::AddRecommendation.new.call(news_id: news.id, donation_id: donation.id)
+          end
+        end
+        Success(Response::ApiResult.new(status: :ok, message: 'Succeed to add recommendations.'))
+      rescue StandardError
+        Failure(Response::ApiResult.new(status: :internal_error, message: 'Fail to add recommendations'))
       end
     end
   end
